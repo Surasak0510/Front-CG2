0e8941415aad0f7094ddd3027d8729dd6b1b5f90
Surasak <92101184+Surasak0510@users.noreply.github.com>
2023-07-20 14:08:17 +0700

    Activity

---
+++ pages/Activity/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="" style="height: calc(100vh - 155px );">
+        <div class="container h-100">
+            <div class="row">
+                <div class="col-12 d-flex align-items-center">
+                    <img src="../../static/Logo.png" alt="" style="width: 20vh;">
+                    <strong><h2 class="m-0 fw-bold" style="color: #00CC99;">Carbon Reward</h2></strong>
+                </div>
+            </div>
+            <div class="row rounded-5 p-5" style="background-color: #ECF0EE;">
+                <div class="col-12">
+                    <div class="row rounded-5 p-4 position-relative" style="background-color: #66EDCB;  border: 5px solid white;">
+                        <div class="col-3 d-flex justify-content-center">
+                            <img :src="Store.img" alt="" style="width: 10vh;">
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <h1>{{ Store.name }}</h1>
+                                <p>{{ Store.loc }}</p>
+                            </div>
+                        </div>
+                        <p class="position-absolute text-end" style="top: 1vh; right: 2vh;">ID : {{ Store.ID }}</p>
+                    </div>
+                    
+                    <div class="row my-4" style="border-bottom: 1px solid #A19999;">
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="row">
+                                <a class="btn p-4 rounded-pill" href="/activity/type" role="button" style="background-color: #5EEECA;"><h1 class="fw-bold" style="color: #0A755A;">Collect points</h1></a>
+                            </div>
+                            <div class="row my-5 position-relative" style="border-bottom: 1px solid #A19999;">
+                                <p class="fs-4 position-absolute" style="background-color: #ECF0EE; width: 5vh; top: -15px; right: 47%;">OR</p>    
+                            </div>
+                            <div class="row">
+                                <a class="btn p-4 rounded-pill" href="#" role="button" style="background-color: #FAF7B8;"><h1 class="fw-bold" style="color: #0A755A;">Redeem points</h1></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return{
+            Store: {
+                name: "Cafe Amazon",
+                img : "https://img.ws.mms.shopee.co.th/62c71069559c4b983b9e2c6d89df14b7",
+                loc : "89,89/1-89/3 Highway No. 3, Sai Mai Subdistrict, Sai Mai District, Bangkok, Bangkok 10220",
+                ID : "100516"
+            }
+        }
+    },
+    mounted() {
+        console.log(this.$route.query.ID)
+    }
+}
+</script>
---
+++ pages/Activity/type.vue
@@ -0,0 +1,103 @@
+<template>
+    <div>
+        <div v-if="type === undefined" style="height: calc(100vh - 155px);">
+            <div class="container h-100">
+                <div class="col-12 h-100">
+                    <div class="row d-flex justify-content-center">
+                        <img src="../../static/Logo.png" alt="" style="width: 25vh;">
+                    </div>
+                    <!-- <div class="row">
+                        <h1 class="text-center fw-bold" style="color: #00CC99;">Select Type</h1>
+                    </div> -->
+                    <div class="row d-flex justify-content-center">
+                        <p class="text-center fs-2">Nodata...</p>
+                        <!-- <div class="col-5 rounded-5 m-3 p-0" v-for="(item, index) in type" :key="index"
+                            style="background-color: #CCF5EA; height: 300px;">
+                            <div class="row rounded-5 w-100 d-flex justify-content-center p-0 m-0"
+                                style="background-color: #00CC99;">
+                                <img class="" :src="item.img" alt="" style="width: 25vh; ">
+                            </div>
+                            <h5 class="text-center my-3">{{ item.desc }}</h5>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container h-100" v-else>
+            <div class="col-12 h-100">
+
+                <div class="row d-flex justify-content-center">
+                    <img src="../../static/Logo.png" alt="" style="width: 25vh;">
+                </div>
+
+                <div class="row">
+                    <h1 class="text-center fw-bold" style="color: #00CC99;">Select Type</h1>
+                </div>
+
+                <div class="row d-flex justify-content-center mb-5 align-self-stretch">
+                    <div class="col-5 rounded-5 m-3 p-0 form-check form-switch" v-for="(item, index) in type" :key="index" style="background-color: #CCF5EA;">
+                        <label class="btn btn w-100 rounded-5 p-0 m-0" for="btn-check" style="border: 1px solid white;">
+                            <div class="row rounded-5 w-100 d-flex justify-content-center p-0 m-0"
+                            style="background-color: #00CC99;">
+                            <img v-if="item.img === ''" src="../../static/Logo.png" alt="" style="width: 25vh;">
+                            <img v-else class="" :src="item.img" alt="" style="width: 25vh; ">
+                        </div>
+                        <h5 v-if="item.desc === ''" class="text-center my-3">Nodata...</h5>
+                        <h5 v-else class="text-center my-3">{{ item.desc }}</h5>
+                        <!-- <input type="checkbox" id="yes" name="yes" :value="item.Item_id" v-model="check" /> -->
+                        </label>
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" :value="item.Item_id" v-model="check" style="top: 1vh; right: 2vh; width: 2vw; height: 2vh;">
+                    </div>
+                </div>
+                {{ check }}
+                <div class="row mb-5 d-flex justify-content-end">
+                    <button type="button" class="btn btn-success w-25 rounded-5" style="background-color: #00CC99; color: white; ">Next</button>
+                </div>
+
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Enable body scrolling</button>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Try scrolling the rest of the page to see this option in action.</p>
+  </div>
+</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            check: [],
+            type:[
+                {
+                    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQjBtmD2IIHrxQC4NiVg458a9gK8JgIRXtsw&usqp=CAU",
+                    desc: "123",
+                    Item_id: "1001"
+                },
+                {
+                    img: "",
+                    desc: "",
+                    Item_id: "1002"
+                },
+                {
+                    img: "",
+                    desc: "",
+                    Item_id: "1003"
+                },
+                {
+                    img: "",
+                    desc: "",
+                    Item_id: "1004"
+                },
+            ]
+        }
+    },
+}
+</script>
